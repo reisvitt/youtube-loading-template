410192ad54798306553e16e056c2dc2c2a3ab17b
reisvitt <reis_vitor@outlook.com>
2020-03-23 22:29:52 -0300

    initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './styles.css'
+
+const Card = () => (
+  <>
+    <div className="card">
+      <div className="video" />
+      <div className="detalhes-video">
+        <div className="icone-canal"/>
+        <div className="video-info">
+          <div className="title-video"/>
+          <div className="nome-canal"/>
+        </div>
+      </div>
+    </div>
+  </>
+)
+
+export default Card
